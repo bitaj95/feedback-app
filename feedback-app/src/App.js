d87f8bfb460b2828a1f813a4755bdbe505b57cab
Bita <bita.janzadeh@hotmail.com>
2022-06-18 01:16:13 -0400

    Create & import FeedbackList component -- empty for now

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -1,5 +1,19 @@
+import React, { useState } from "react";
+import Header from "./components/Header";
+import FeedbackData from "./data/FeedBackData";
+import FeedbackList from "./components/FeedbackList";
+
 function App() {
-  return <h1>hellloooo from APP</h1>;
+  const [feedback, setFeedback] = useState(FeedbackData);
+
+  return (
+    <>
+      <Header text="hello work" />
+      <div className="container">
+        <FeedbackList />
+      </div>
+    </>
+  );
 }
 
 export default App;
---
+++ feedback-app/src/components/FeedbackList.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function FeedbackList() {
+  return <div>FList</div>;
+}
+
+export default FeedbackList;
