61ddfc500bc0b7579516dea8e013d44aed99a0e8
Bita <bita.janzadeh@hotmail.com>
2022-06-18 19:59:46 -0400

    Create FeedbackStats component that calculates average of current ratings

---
+++ feedback-app/src/components/FeedbackStats.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function FeedbackStats({ feedback }) {
+  //Calculate rating averagee
+
+  let average =
+    feedback.reduce((acc, cur) => {
+      return acc + cur.rating;
+    }, 0) / feedback.length;
+
+  return (
+    <div className="feedback-stats">
+      <h4> {feedback.length} Reviews</h4>
+      <h4> Average Rating: {average}</h4>
+    </div>
+  );
+}
